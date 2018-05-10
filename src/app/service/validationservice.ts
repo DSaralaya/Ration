4f3bd64f5c02e9a0d6c75a5f545f83b8d94ca9a1
deepaksaralaya <dpk.saralaya@gmail.com>
2018-05-10 18:27:47 +0530

    Error Msg Changes

--- src/app/service/validationservice.ts
+++ src/app/service/validationservice.ts
@@ -2,7 +2,7 @@ export class ValidationService {
     static getValidatorErrorMessage(validatorName: string, validatorValue?: any,control?:any) {
         debugger;
         let config = {
-            'required': 'field is required',
+            'required': 'Field is Required',
             'invalidCreditCard': 'Is invalid credit card number',
             'invalidPassword': 'Invalid password. Password must be at least 6 characters long, and contain a number.',
             'minlength': `Minimum length ${validatorValue.requiredLength}`,
